295e6ed84dd4ff57b632b04bb6e30368fff972f8
Xbendit <vanrossumben@gmail.com>
2024-11-29 15:49:20 +0100

    commit2

--- src/App.jsx
+++ src/App.jsx
@@ -17,103 +17,45 @@ function App() {
         setKiwis(0)
     }
 
-    const[Formstate, setFormstate] = useState({
-        firstname:'',
-        lastname:'',
-        age:0,
-        postcode:'',
-        momemt:'day',
 
+    const [Formstate, setFormstate] = useState({
+        firstname: '',
+        lastname: '',
+        age: 0,
+        postcode: '',
+        moment: '',
+        dropoff: 'Iedere week',
+        opmerking: '',
+        avw: '',
 
 
     });
 
-    function handleChange(e){
-        const changeFieldName= e.target.name;
-        setFormstate((prevState)=>({
+    function handleChange(e) {
+        /*const changeFieldName = e.target.name;*/
+        const { name, value, type, checked } = e.target;
+        setFormstate((prevState) => ({
             ...prevState,
-           [changeFieldName]: e.target.value,
+            /*[changeFieldName]: e.target.value,*/
+            [name]: type === "checkbox" ? checked : value,
         }));
+    }
+
+    function handleClicksent() {
+        console.log("Voornaam:", Formstate.firstname)
+        console.log("Achternaam:", Formstate.lastname)
+        console.log("Leeftijd:", Formstate.age)
+        console.log("Postcode:", Formstate.postcode)
+        console.log("Wanneer:", Formstate.dropoff)
+        console.log("Moment:", Formstate.moment)
+        console.log("Opmerking:", Formstate.opmerking)
+        console.log("avw:", Formstate.avw)
+
     }
 
     return (
         <>
             <article>
-                <div>
-                    <label>Voornaam</label>
-                    <input
-                        type="text"
-                        name="firstname"
-                        value={Formstate.firstname}
-                        onChange={(e)=>{
-                            console.log(`Firstname: ${e.target.value}`);
-                            handleChange(e);
-                        }}
-                    />
-                </div>
-                <div>
-                    <label>Achternaam</label>
-                    <input
-                        type="text"
-                        name="lastname"
-                        value={Formstate.lastname}
-                        onChange={(e)=>{
-                            console.log(`Lastname: ${e.target.value}`);
-                            handleChange(e);
-                        }}
-                    />
-                </div>
-                <div>
-                    <label>Leeftijd</label>
-                    <input
-                        type="text"
-                        name="age"
-                        value={Formstate.age}
-                        onChange={(e) => {
-                            console.log(`Age: ${e.target.value}`);
-                            handleChange(e);
-                        }}
-                    />
-                </div>
-                <div>
-                    <label>Postcode</label>
-                    <input
-                        type="text"
-                        name="postcode"
-                        value={Formstate.postcode}
-                        onChange={(e) => {
-                            console.log(`Postcode: ${e.target.value}`);
-                            handleChange(e);
-                        }}
-                    />
-                </div>
-                <div>
-                    <h3>Bezorgfrequentie</h3>
-                </div>
-                <div>
-                    <div>
-                        <input type="radio" id="huey" name="moment" value="day" onClick={ (e)=>{
-                            console.log(`Moment:${e.target.value}`);
-                            handleChange(e);
-                        }}/>
-                        <label htmlFor="huey">Overdag</label>
-                    </div>
-                    <div>
-                        <input type="radio" id="dewey" name="moment" value="night" onClick={ (e)=> {
-                            console.log(`Moment:${e.target.value}`);
-                            handleChange(e);
-                        }}/>
-                        <label htmlFor="dewey">'s Avonds</label>
-                    </div>
-                </div>
-                <div>
-                    <h3>Opmerking</h3>
-                </div>
-                <div>
-                    <h3>Ik ga akkoord met de voorwaarden</h3>
-                </div>
-            </article>
-            {/*<article>
                 <h1>Fruitmand bezorgservice</h1>
                 <div>
                     <h2>Aardbeien</h2>
@@ -180,7 +122,115 @@ function App() {
                     </button>
                 </div>
                 <button onClick={handleClickreset}>Reset</button>
-            </article>*/}
+            </article>
+            <article>
+                <div>
+                    <label>Voornaam</label>
+                    <input
+                        type="text"
+                        name="firstname"
+                        value={Formstate.firstname}
+                        onChange={(e) => {
+                            handleChange(e);
+                        }}
+                    />
+                </div>
+                <div>
+                    <label>Achternaam</label>
+                    <input
+                        type="text"
+                        name="lastname"
+                        value={Formstate.lastname}
+                        onChange={(e) => {
+                            handleChange(e);
+                        }}
+                    />
+                </div>
+                <div>
+                    <label>Leeftijd</label>
+                    <input
+                        type="text"
+                        name="age"
+                        value={Formstate.age}
+                        onChange={(e) => {
+                            handleChange(e);
+                        }}
+                    />
+                </div>
+                <div>
+                    <label>Postcode</label>
+                    <input
+                        type="text"
+                        name="postcode"
+                        value={Formstate.postcode}
+                        onChange={(e) => {
+                            handleChange(e);
+                        }}
+                    />
+                </div>
+                <div>
+                    <h3>Bezorgfrequentie</h3>
+                    <select
+                        name="dropoff"
+                        value={Formstate.dropoff}
+                        onChange={(e) => {
+                            handleChange(e);
+                        }}>
+                        <option value="Iedere week">Iedere week</option>
+                        <option value="Om de week">Om de week</option>
+                        <option value="Iedere maand">Iedere maand</option>
+                    </select>
+                </div>
+                <div>
+                    <div>
+                        <input type="radio"
+                               id="huey"
+                               name="moment"
+                               value="day"
+                               checked={Formstate.moment === "day"}
+                               onChange={(e) => {
+                                   handleChange(e);
+                               }}/>
+                        <label htmlFor="huey">Overdag</label>
+                    </div>
+                    <div>
+                        <input
+                            type="radio"
+                            id="dewey"
+                            name="moment"
+                            value="night"
+                            checked={Formstate.moment === "night"}
+                            onChange={(e) => {
+                                handleChange(e);
+                            }}/>
+                        <label htmlFor="dewey">'s Avonds</label>
+                    </div>
+                </div>
+                <div>
+                    <label>Opmerking</label>
+                    <textarea
+                        type="text"
+                        name="opmerking"
+                        value={Formstate.opmerking}
+                        onChange={(e) => {
+                            handleChange(e);
+                        }}
+                    />
+                </div>
+                <div>
+                    <div>
+                        <input type="checkbox"
+                               id="voorwaarde"
+                               name="avw"
+                               checked={Formstate.checked}
+                               onClick={(e) => {
+                                   handleChange(e);
+                               }}/>
+                        <label htmlFor="voorwaarde">Ik ga akkoord met de voorwaarde</label>
+                    </div>
+                </div>
+                <button onClick={handleClicksent}>Versturen</button>
+            </article>
         </>
     )
 }
